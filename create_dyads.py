6063d03e2ca4dbcf7d2bb4e38cfd4bdf8ca98e8e
fynnomenon <82415981+fynnomenon@users.noreply.github.com>
2022-09-09 12:59:09 +0200

    update: seperate individuals preperation and dyads creation

---
+++ create_dyads.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# import the necessary packages
+import moviepy.editor as mpy
+import numpy as np
+import pandas as pd
+import cv2
+from tqdm.auto import tqdm
+from os import mkdir, listdir
+from os.path import exists, join
+from re import search
+from crop_and_resize import get_bbox_coords
+
+def change_dur(clip, duration):
+    rate = clip.fps
+    dur = clip.duration
+
+    desired_rate = (rate*dur)/duration
+    scaling_factor = desired_rate/rate
+
+    # modify the FPS
+    clip = clip.set_fps(clip.fps*scaling_factor)
+    # apply speed up
+    clip = clip.fx(mpy.vfx.speedx,scaling_factor)
+
+    return clip
+
+def calculate_center(clip):
+    x,y,w,h = get_bbox_coords(clip)
+    center = (round((2*x+w)/2),round((2*y+h)/2))
+
+    return center
+
+def create_gesture_combinations(pairings, files, is_1G=False):
+    for ind, pair in enumerate(pairings):
+        gestures_left = []
+        gestures_right = []
+        if is_1G: # for the 1G condition only extract one gesture per side (the other side will be 'gg00' in each case)
+            regex_left = rf'({pair[0]})_(f01|p)'
+            regex_right = rf'({pair[1]})_(f02|n)'
+        else: # for 2G condition extract both gesture per side (to allow for SAME and DIFF combinations)
+            regex_left = rf'({pair[0]}|{pair[1]})_(f01|p)'
+            regex_right = rf'({pair[0]}|{pair[1]})_(f02|n)'
+
+        # get the filenames for the gestures to be paired
+        for f in files:
+            if search(regex_left, f):
+                gestures_left.append(f)
+            elif search(regex_right, f):
+                gestures_right.append(f)
+        # calculate all desired combinations for the respective pairing
+        pairings[ind] = [(left,right) for right in gestures_right for left in gestures_left]
+
+    return pairings
+
+def determine_interaction_type(left, right):
+    # from the naming of the individual stimuli files determine which interaction type the output file will have
+    if len(left[33:-4]) != len(right[33:-4]):
+        return 'HR'
+    elif len(left[33:-4]) == 1:
+        return 'RR'
+    else:
+        return 'HH'
+
+def determine_gesture_type(left, right):
+    # from the naming of the individual stimuli files determine which gesture type the output file will have
+    if left[28:32] != right[28:32]:
+        return '2G_DIFF'
+    elif left[28:32] == 'gg00':
+        return '1G'
+    else:
+        return '2G_SAME'
+
+def main():
+    in_dir = 'IndividualStimuliCleaned'
+    files = [join(in_dir, f) for f in listdir(in_dir)]
+
+    root_dir = 'DyadStimuli'
+    sub_dirs = ['1G', '2G_SAME', '2G_DIFF'] # Single-passive-gestures; Two-identical-gestures; Two-different-gestures (gesture types)
+    sub_sub_dirs = ['HH','HR','RR'] # Human-Human-Interaction; Human-Robot-Interaction; Robot-Robot-Interaction (interaction types)
+
+    # create the folder structure of the output directory
+    is_existend = exists(root_dir)
+    if not is_existend:
+        mkdir(root_dir)
+        for sub_dir in sub_dirs:
+            path_sub_dir = join(root_dir,sub_dir)
+            mkdir(path_sub_dir)
+            for sub_sub_dir in sub_sub_dirs:
+                path_sub_sub_dir = join(root_dir,sub_dir,sub_sub_dir)
+                mkdir(path_sub_sub_dir)
+
+    # get all congruent 2G pairings
+    pairings_2G = [('gg09', 'gg19'), ('gg01', 'gg18'), ('gg10', 'gg11'), ('gg13', 'gg17'),
+                   ('gg08', 'gg20'), ('gg04', 'gg15'), ('gg02', 'gg06'), ('gg03', 'gg07'),]
+
+    # get all 1G pairings
+    all_gestures_df = pd.read_excel('GesturesIndividualStimuli.xlsx', 'gestures_combinations')
+    relevant_gestures_df = all_gestures_df.dropna()
+    gesture_codes = relevant_gestures_df['GestureCode'].tolist()
+
+    pairings_1G = []
+    for i in gesture_codes[1:]:
+        single_gesture = ('gg00', i)
+        pairings_1G.append(single_gesture)
+
+    # create all combinations for the 1G and 2G pairings
+    combinations_2G = create_gesture_combinations(pairings_2G, files)
+    combinations_1G = create_gesture_combinations(pairings_1G, files, True)
+    all_combinations = combinations_2G + combinations_1G
+    all_combs_flat = [item for sublist in all_combinations for item in sublist]
+
+    for gestures in tqdm(all_combs_flat):
+        # determine the interaction and gesture type of the dyad stimuli to be created
+        interaction_type = determine_interaction_type(gestures[0],gestures[1])
+        gesture_type = determine_gesture_type(gestures[0],gestures[1])
+
+        # load the video files of the individual stimuli to be combined into a dyad
+        gesture_left = mpy.VideoFileClip(gestures[0],has_mask=True)
+        gesture_right = mpy.VideoFileClip(gestures[1],has_mask=True).fx(mpy.vfx.mirror_x)
+
+        # calculate the center of gravity of the human body in the resized clip
+        center_left = calculate_center(gesture_left)
+        center_right = calculate_center(gesture_right)
+
+        # speed up or slow down video to 3sec
+        duration = 3
+        gesture_right = change_dur(gesture_right,duration)
+        gesture_left = change_dur(gesture_left,duration)
+
+        # load the background image
+        background = mpy.ImageClip('background_dyads.png',duration=duration)
+
+        # create the video composition
+        dyad = mpy.CompositeVideoClip([background,
+                                       gesture_left.set_position((660-center_left[0],background.size[1]-gesture_left.size[1]-170)),
+                                       gesture_right.set_position((1260-center_right[0],background.size[1]-gesture_right.size[1]-170))
+                                      ])
+        # make the video grayscale
+        dyad = dyad.fx(mpy.vfx.blackwhite)
+
+        # write the output video file
+        out_path = join(root_dir,gesture_type,interaction_type,f'DS_l{gestures[0][28:-4]}_r{gestures[1][28:-4]}.mp4')
+        dyad.write_videofile(out_path,fps=25,audio=False, verbose=False, logger=None)
+
+if __name__ == "__main__":
+    main()
---
+++ crop_and_resize.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# import necessary packages
+import moviepy.editor as mpy
+import subprocess
+import numpy as np
+import cv2
+from tqdm.auto import tqdm
+from tqdm.contrib import tzip
+from os import mkdir, listdir
+from os.path import exists, join
+
+def remove_outliers(src):
+    num_labels,labels,stats,centroids = cv2.connectedComponentsWithStats(src,connectivity=4,ltype=None)
+    img = np.zeros((src.shape[0],src.shape[1]),np.uint8) # create a black background of all 0
+    for i in range(1,num_labels):
+        mask = labels == i # this step is to determine the location of the area through labels, assign labels information to the mask array, and then use the mask array as the index of img array
+        if stats[i][4] > 100:
+            img[mask] = 255 # areas larger than 100 shall be painted white, and areas smaller than 100 shall be painted black
+        else:
+            img[mask] = 0
+    return img
+
+def get_contours(frm):
+    # convert frame to grayscale
+    frm_gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
+    # remove tiny particels
+    frm_out = remove_outliers(frm_gray)
+    # convert frame to black and white
+    frm_thresh = cv2.threshold(frm_out,0,255,cv2.THRESH_BINARY)[1]
+    # extract contours
+    contours,_ = cv2.findContours(frm_thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    return contours
+
+def get_bbox_coords(clip, t=0):
+    x_bbox=y_bbox=w_bbox=h_bbox=0
+    # get the frame
+    frm = clip.get_frame(t)
+    # get the bounding box of the frame
+    contours = get_contours(frm)
+    # get the coordinates of the bounding box
+    for cnt in contours:
+        x,y,w,h = cv2.boundingRect(cnt)
+        if w > 100 and h > 100: # only consider rectangles of relevant size
+            x_bbox,y_bbox,w_bbox,h_bbox=x,y,w,h
+
+    return x_bbox,y_bbox,w_bbox,h_bbox
+
+def crop_and_resize(clip):
+    clip_x, clip_y = clip.size
+    bbox = [clip_x,0,clip_y,0]
+    average_h = 740
+
+    # iterate through frames of the video clip
+    for frm in clip.iter_frames():
+        contours = get_contours(frm)
+
+        # iterate over contours
+        for cnt in contours:
+            x,y,w,h = cv2.boundingRect(cnt)
+            # save rectangle that contains all boundingboxes inside it
+            if x < bbox[0]:
+                bbox[0] = x
+            if x+w > bbox[1]:
+                bbox[1] = x+w
+            if y < bbox[2]:
+                bbox[2] = y
+            if y+h > bbox[3]:
+                bbox[3] = y+h
+
+    # crop and resize the clip
+    _,_,_,clip_h = get_bbox_coords(clip)
+    scaling_factor =  average_h/clip_h
+
+    cropped_clip = clip.fx(mpy.vfx.crop, x1=bbox[0],x2=bbox[1],y1=bbox[2],y2=bbox[3])
+    resized_clip = cropped_clip.resize(scaling_factor)
+
+    return resized_clip
+
+def fix_rotation(clip):
+    # Workaround for the moviepy bug causing videos with rotation metadata to be stretched
+    if clip.rotation in (90,270):
+            clip = clip.resize(clip.size[::-1])
+            clip.rotation = 0
+
+    return clip
+
+def main():
+    in_dir = 'IndividualStimuli'
+    file_names = listdir(in_dir)
+    file_paths = [join(in_dir, f) for f in file_names]
+
+    out_dir = 'IndividualStimuliCleaned'
+    is_existend = exists(out_dir)
+    if not is_existend: # create the folder structure of the output directory
+        mkdir(out_dir)
+
+    seq_dir = 'ImageSequences'
+    is_existend = exists(seq_dir)
+    if not is_existend: # create a folder for the image sequences
+        mkdir(seq_dir)
+
+    for f_name,f_path in tzip(file_names,file_paths):
+        # load the video files of the individual stimuli to be cleaned
+        clip = mpy.VideoFileClip(f_path, has_mask=True)
+        # Workaround for the moviepy bug causing videos with rotation metadata to be stretched
+        temp_clip = fix_rotation(clip)
+        # transform the individual stimuli files
+        cleaned_clip = crop_and_resize(temp_clip)
+
+        # Workaround for the moviepy bug that write_videofile() cannot export a video clip with an alpha channel
+        img_seq_path = join(seq_dir, f_name[:-4])
+        mkdir(img_seq_path)
+        fps = cleaned_clip.fps
+        cleaned_clip.write_images_sequence(f'{img_seq_path}/frame%04d.png', fps=fps, withmask=True, verbose=False, logger=None) # create image sequence of cleaned video
+
+        command = f'ffmpeg -i {img_seq_path}/frame%4d.png -framerate {fps} -pix_fmt yuva444p10le -vcodec prores_ks -threads 6 {out_dir}/{f_name}' # create transparent video out of image sequence
+        subprocess.call(command, shell=True)
+
+if __name__ == "__main__":
+    main()
